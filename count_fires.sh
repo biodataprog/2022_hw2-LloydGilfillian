1061120f5cf7f2ef97275122ce6756f115aaa728
LloydGilfillian <114544896+LloydGilfillian@users.noreply.github.com>
2022-10-12 11:45:51 -0700

    Update count_fires.sh
    I don't know what I'm doing, I'll study longer

--- count_fires.sh
+++ count_fires.sh
@@ -3,21 +3,34 @@
 # step to write
 # download the CSV file
 # curl -o calfire.csv ...
+lgilf001@pelican:~$ curl -o calfire.csv https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::recent-large-fire-perimeters-5000-acres.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D
+
+lgilf001@pelican:~$ cat calfire.csv
 
 # print out the range of years found -- you may need to go in and edit the file
 # cut -d, ....
-MINYEAR=1900
-MAXYEAR=1901
+lgilf001@pelican:~$ cut -d "," -f 2 
+
+lgilf001@pelican:~$ sort -t “,” -k 2 -r
+
+#MINYEAR=1900
+#MAXYEAR=1901
 # write code to set these variables with the smallest and largest years
+MAXYEAR=2017
+MINYEAR=2021
+
 echo "This report has the years: $MINYEAR-$MAXYEAR"
 # if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
 
 # print out the total number of fires (remember to remove the header line)
+lgilf001@pelican:~$ wc -l
+
 TOTALFILECOUNT=0
 # put your code here to update this variable
 echo "Total number of files: $TOTALFILECOUNT"
 
 # print out the number of fire in each year
+lgilf001@pelican:~$ cut -d "," -f 2
 echo "Number of fires in each year follows:"
 
 
